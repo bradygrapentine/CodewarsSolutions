7bffc2dd82bf6e1b4db4895469ef460f02309834
bradygrapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-05-18 19:35:31 -0400

    Update 7kyu_LargestPairSumInArray.cs

--- 7kyu_LargestPairSumInArray.cs
+++ 7kyu_LargestPairSumInArray.cs
@@ -7,6 +7,5 @@ public class Kata
   {
     var orderedNumbers = numbers.OrderBy(num => num).ToList();
     return orderedNumbers[numbers.Count()-1] + orderedNumbers[numbers.Count()-2];
-    // TODO: Write your code here
   }
 }
