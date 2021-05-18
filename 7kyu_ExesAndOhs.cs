90b7eb6a842291b55660a8928f572b46f66a7d0b
bradygrapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-05-18 18:21:29 -0400

    Update 7kyu_ExesAndOhs.cs

--- 7kyu_ExesAndOhs.cs
+++ 7kyu_ExesAndOhs.cs
@@ -4,3 +4,20 @@ public static class Kata
 {
   public static bool XO (string input) => input.ToLower().Count(chr => chr == 'x') == input.Where(chr => Char.ToLower(chr) == 'o').Count();
 }
+
+// using System.Linq;
+// using System;
+// public static class Kata 
+// {
+//   public static bool XO (string input)
+//   {
+//     if (input.Where(chr => Char.ToLower(chr) == 'x').Count() == input.Where(chr => Char.ToLower(chr) == 'o').Count())
+//     {
+//       return true;
+//     }
+//     else 
+//     {
+//       return false;
+//     }
+//   }
+// }
