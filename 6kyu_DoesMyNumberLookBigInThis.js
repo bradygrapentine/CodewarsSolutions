2b6e9b8298ae674e5ee43029f6da5a3fbbf1aa85
bradygrapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-05-25 13:35:51 -0400

    Create 6kyu_DoesMyNumberLookBigInThis.js

---
+++ 6kyu_DoesMyNumberLookBigInThis.js
@@ -0,0 +1,5 @@
+function narcissistic(num) {
+  return num.toString().split('').reduce((result, dig) => result += (Number(dig) ** num.toString().length), 0) === num
+}
+
+// const narcissistic = num => num.toString().split('').reduce((result, dig) => result += (Number(dig) ** num.toString().length), 0) === num
