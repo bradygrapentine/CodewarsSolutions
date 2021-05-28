f0644ff23b359be8a249ba26f4a68f64cfe3f16e
bradygrapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-05-28 14:47:41 -0400

    Create 5kyu_NumberOfTrailingZerosOfN.js

---
+++ 5kyu_NumberOfTrailingZerosOfN.js
@@ -0,0 +1,11 @@
+
+function zeros (n) {
+  let kMax = Math.floor(Math.log(n) / Math.log(5))
+  let k = 1
+  let result = 0
+  while( k <= kMax) {
+    result += Math.floor(n/5**k)
+    k++
+  }
+  return result
+}
