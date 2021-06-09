3c72da3bce89acc21cdbaf429c4967dc8488152b
bradygrapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-06-09 15:22:06 -0400

    Update 7kyu_ComputerProblemSeries.js

--- 7kyu_ComputerProblemSeries.js
+++ 7kyu_ComputerProblemSeries.js
@@ -1,18 +1,26 @@
 function save(sizes, hd) {
   let count = 0
-  let result = 0
-  for (let i = 0; i < sizes.length; i++){ 
-    if ((result + sizes[i]) <= hd) {
-      result += sizes[i]
-      count++
-      console.log([sizes[i], result, count, hd])
-    }
-    else {
-      break
-    }
+  while ( hd >= 0) {
+    hd -= sizes[count]
+    count++
   }
-  return count
+  return count - 1
 }
 
+// function save(sizes, hd) {
+//   let count = 0
+//   let result = 0
+//   for (let i = 0; i < sizes.length; i++){ 
+//     if ((result + sizes[i]) <= hd) {
+//       result += sizes[i]
+//       count++
+//       console.log([sizes[i], result, count, hd])
+//     }
+//     else {
+//       break
+//     }
+//   }
+//   return count
+// }
 
 // https://www.codewars.com/kata/5d49c93d089c6e000ff8428c/solutions/javascript
