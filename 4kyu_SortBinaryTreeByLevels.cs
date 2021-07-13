e717dc3ef5f35c6b353706c064c4de15941366d0
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-07-13 19:22:32 -0400

    Create 4kyu_SortBinaryTreeByLevels.cs

---
+++ 4kyu_SortBinaryTreeByLevels.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+class Kata
+{
+  public static List<int> TreeByLevels(Node node)
+  {
+    var result = new List<int>();
+    var queue = new Queue<Node>();
+    if (node != null) 
+    {
+      queue.Enqueue(node);
+    }
+    while (queue.Count > 0) 
+    {
+      var n = queue.Dequeue();
+      if (n != null) 
+      {
+        result.Add(n.Value);
+        queue.Enqueue(n.Left);
+        queue.Enqueue(n.Right);
+      }
+    }
+    return result;
+  }
+}
+
+https://www.codewars.com/kata/52bef5e3588c56132c0003bc
