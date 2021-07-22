384ebb83af9864a68e8bc3774110182b46851309
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-07-22 17:45:43 -0400

    Update 6kyu_AckermannFunction.js

--- 6kyu_AckermannFunction.js
+++ 6kyu_AckermannFunction.js
@@ -4,19 +4,19 @@ Ackermann = function(m,n) {
   else return Ackermann(m-1, Ackermann(m, n-1))
 }
 
-Ackermann = function(m,n) {
-  if (m === 0) {
-    return n+1
-  }
-  else if (m > 0 && n === 0) {
-    return Ackermann(m-1, 1)
-  }
-  else if (m > 0 && n > 0) {
-    return Ackermann(m-1, Ackermann(m, n-1))
-  }
-  else {
-    return null
-  }
-}
+// Ackermann = function(m,n) {
+//   if (m === 0) {
+//     return n+1
+//   }
+//   else if (m > 0 && n === 0) {
+//     return Ackermann(m-1, 1)
+//   }
+//   else if (m > 0 && n > 0) {
+//     return Ackermann(m-1, Ackermann(m, n-1))
+//   }
+//   else {
+//     return null
+//   }
+// }
 
 // https://www.codewars.com/kata/53ad69892a27079b34000bd9
