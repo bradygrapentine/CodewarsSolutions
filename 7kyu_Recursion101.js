5fc311a91abe038993e2020f78bad074141ee4f6
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-07-22 17:26:22 -0400

    Create 7kyu_Recursion101.js

---
+++ 7kyu_Recursion101.js
@@ -0,0 +1,18 @@
+function solve(a,b){
+  if (a === 0 || b === 0) {
+    return [a,b]
+  }
+  else if (a >= (2*b)) {
+    a = a-(2*b)
+    return solve(a, b)
+  }
+  else if (b >= (2*a)) {
+    b = b-(2*a)
+    return solve(a, b)
+  }
+  else {
+    return [a,b]
+  }
+}
+
+// https://www.codewars.com/kata/5b752a42b11814b09c00005d
