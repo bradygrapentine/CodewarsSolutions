cbfdb4e5c38fba6424422c93b30272c3d71ee57d
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-07-23 12:03:57 -0400

    Update 6kyu_SumOfDigitsDigitalRoot.cs

--- 6kyu_SumOfDigitsDigitalRoot.cs
+++ 6kyu_SumOfDigitsDigitalRoot.cs
@@ -10,26 +10,26 @@ public class Number
   }
 }
 
-using System;
-using System.Linq;
+// using System;
+// using System.Linq;
 
-public class Number
-{
-  public int DigitalRoot(long n)
-  {
-    while(n.ToString().Length > 1) {
-      var nAsString = n.ToString();
-      var nStrSplt = nAsString.ToArray();
-      var result = 0;
-      foreach(var dig in nStrSplt)
-      {
-        var digAsNum = Int32.Parse(dig.ToString());
-        result += digAsNum;
-      }
-      n = result;
-    }
-    return Convert.ToInt32(n);
-  }
-}
+// public class Number
+// {
+//   public int DigitalRoot(long n)
+//   {
+//     while(n.ToString().Length > 1) {
+//       var nAsString = n.ToString();
+//       var nStrSplt = nAsString.ToArray();
+//       var result = 0;
+//       foreach(var dig in nStrSplt)
+//       {
+//         var digAsNum = Int32.Parse(dig.ToString());
+//         result += digAsNum;
+//       }
+//       n = result;
+//     }
+//     return Convert.ToInt32(n);
+//   }
+// }
 
 // https://www.codewars.com/kata/541c8630095125aba6000c00
