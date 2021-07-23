91eee7253b3dce236c8f77289048687be5edbd9a
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-07-23 12:03:35 -0400

    Create 6kyu_SumOfDigitsDigitalRoot.cs

---
+++ 6kyu_SumOfDigitsDigitalRoot.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Linq;
+
+public class Number
+{
+  public int DigitalRoot(long n)
+  {
+    var result = n.ToString().ToArray().Select(dig => Int32.Parse(dig.ToString())).Sum();
+    return result.ToString().Length > 1 ? DigitalRoot(Convert.ToInt64(result)) : Convert.ToInt32(result);
+  }
+}
+
+using System;
+using System.Linq;
+
+public class Number
+{
+  public int DigitalRoot(long n)
+  {
+    while(n.ToString().Length > 1) {
+      var nAsString = n.ToString();
+      var nStrSplt = nAsString.ToArray();
+      var result = 0;
+      foreach(var dig in nStrSplt)
+      {
+        var digAsNum = Int32.Parse(dig.ToString());
+        result += digAsNum;
+      }
+      n = result;
+    }
+    return Convert.ToInt32(n);
+  }
+}
+
+// https://www.codewars.com/kata/541c8630095125aba6000c00
