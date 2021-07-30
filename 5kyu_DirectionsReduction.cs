201fc44b149fe6f7ba9c3ed348e84c17fc0b1754
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-07-30 11:12:23 -0400

    Update 5kyu_DirectionsReduction.cs

--- 5kyu_DirectionsReduction.cs
+++ 5kyu_DirectionsReduction.cs
@@ -8,16 +8,10 @@ public class DirReduction {
       for (var i = 0; i < arr.Count(); i++) 
       {
         if (result.Count() != 0) {
-          if (result[result.Count() - 1] == "NORTH" && arr[i] == "SOUTH") {
-            result.RemoveAt(result.Count() - 1);
-          }
-          else if (result[result.Count() - 1] == "SOUTH" && arr[i] == "NORTH") {
-            result.RemoveAt(result.Count() - 1);
-          } 
-          else if (result[result.Count() - 1] == "EAST" && arr[i] == "WEST") {
-            result.RemoveAt(result.Count() - 1);
-          }
-          else if (result[result.Count() - 1] == "WEST" && arr[i] == "EAST") {
+          if ((result[result.Count() - 1] == "NORTH" && arr[i] == "SOUTH") || 
+          (result[result.Count() - 1] == "SOUTH" && arr[i] == "NORTH") || 
+          (result[result.Count() - 1] == "EAST" && arr[i] == "WEST") ||
+          (result[result.Count() - 1] == "WEST" && arr[i] == "EAST")) {
             result.RemoveAt(result.Count() - 1);
           }
           else {
@@ -31,6 +25,40 @@ public class DirReduction {
       return result.ToArray();
     }
 }
+
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// public class DirReduction {
+  
+//     public static string[] dirReduc(String[] arr) {
+//       var result = new List<string>();
+//       for (var i = 0; i < arr.Count(); i++) 
+//       {
+//         if (result.Count() != 0) {
+//           if (result[result.Count() - 1] == "NORTH" && arr[i] == "SOUTH") {
+//             result.RemoveAt(result.Count() - 1);
+//           }
+//           else if (result[result.Count() - 1] == "SOUTH" && arr[i] == "NORTH") {
+//             result.RemoveAt(result.Count() - 1);
+//           } 
+//           else if (result[result.Count() - 1] == "EAST" && arr[i] == "WEST") {
+//             result.RemoveAt(result.Count() - 1);
+//           }
+//           else if (result[result.Count() - 1] == "WEST" && arr[i] == "EAST") {
+//             result.RemoveAt(result.Count() - 1);
+//           }
+//           else {
+//             result.Add(arr[i]);
+//           }
+//         }
+//         else {
+//           result.Add(arr[i]);
+//         }
+//       }
+//       return result.ToArray();
+//     }
+// }
 
 
 // https://www.codewars.com/kata/550f22f4d758534c1100025a
