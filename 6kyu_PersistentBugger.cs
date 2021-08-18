30171885097890b946c084ca9f3f26a5e96ba7ca
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-08-18 16:18:18 -0400

    Create 6kyu_PersistentBugger.cs

---
+++ 6kyu_PersistentBugger.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+
+public class Persist 
+{
+	public static int Persistence(long n) 
+	{
+    var count = 0;
+    var searching = n.ToString().Length > 1 ? true : false;
+    while (searching) 
+    {
+      var spltInput = n.ToString().ToCharArray();
+      n = 1;
+      foreach (var dig in spltInput) 
+      {
+        n *= Int32.Parse(dig.ToString());
+      }
+      count++;
+      if (n < 10) 
+      {
+        searching = false;
+      }
+    }
+    return count;
+  }
+}
+
+// https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec
