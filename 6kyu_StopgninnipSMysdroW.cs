c316492c337a5803a52f4a67b69a640e0aac39c2
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-08-17 22:19:18 -0400

    Create 6kyu_StopgninnipSMysdroW.cs

---
+++ 6kyu_StopgninnipSMysdroW.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class Kata
+{
+  public static string SpinWords(string sentence)
+  {
+    return String.Join(" ", sentence
+                            .Split(' ')
+                            .Select(word => 
+                                    word.Length > 4 ? 
+                                    new string(word.Reverse().ToArray()) : 
+                                    word));
+  }
+}
+
+// https://www.codewars.com/kata/5264d2b162488dc400000001
