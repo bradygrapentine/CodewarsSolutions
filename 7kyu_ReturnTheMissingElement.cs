e0257928d7907b4b6d8372b5c18d5756a3fc327d
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-08-26 16:41:39 -0400

    Update 7kyu_ReturnTheMissingElement.cs

--- 7kyu_ReturnTheMissingElement.cs
+++ 7kyu_ReturnTheMissingElement.cs
@@ -3,10 +3,21 @@ using System.Linq;
 
 public static class Kata
 {
-  public static int GetMissingElement(int[] superImportantArray)
+  public static int GetMissingElement(int[] superImportantArray, int lastVal = 9)
   {
-    return 45 - superImportantArray.Sum();
+    return Enumerable.Range(1, lastVal).Sum() - superImportantArray.Sum();  
   }
 }
+
+// using System;
+// using System.Linq;
+
+// public static class Kata
+// {
+//   public static int GetMissingElement(int[] superImportantArray)
+//   {
+//     return 45 - superImportantArray.Sum();
+//   }
+// }
 
 // https://www.codewars.com/kata/5299413901337c637e000004
