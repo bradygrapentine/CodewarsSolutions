ac891d86a0e19f5cfcb611bd24b1adb68fd93f27
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-08-28 14:32:39 -0400

    Update 6kyu_CamelCaseMethod.js

--- 6kyu_CamelCaseMethod.js
+++ 6kyu_CamelCaseMethod.js
@@ -1,21 +1,25 @@
 String.prototype.camelCase=function() {
-      let str = Object.values(this).join('')
-      if (str === null) {
-        return null
-      } else if (str === "") {
-        return str
-      }
-      str = str.trim().split('')
-      for (let i = 0; i < str.length; i++) {
-        if (i === 0) {
-          str[i] = str[i].toUpperCase()
-        }
-        else if (str[i] === ' ' && i !== str.length-1) {
-          str.splice(i, 2, str[i+1].toUpperCase())
-        }   
-      }
-      return str.join('')
-    }
+  return this == "" || this == null ? Object.values(this).join('') : this.trim().split(' ').map(c => c[0].toUpperCase() + c.slice(1)).join('')
+}
+
+// String.prototype.camelCase=function() {
+//       let str = Object.values(this).join('')
+//       if (str === null) {
+//         return null
+//       } else if (str === "") {
+//         return str
+//       }
+//       str = str.trim().split('')
+//       for (let i = 0; i < str.length; i++) {
+//         if (i === 0) {
+//           str[i] = str[i].toUpperCase()
+//         }
+//         else if (str[i] === ' ' && i !== str.length-1) {
+//           str.splice(i, 2, str[i+1].toUpperCase())
+//         }   
+//       }
+//       return str.join('')
+//     }
 
 // Object.defineProperty(String.prototype, 'camelCase', {
 //     value() {
