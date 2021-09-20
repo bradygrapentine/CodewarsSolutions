49e9fe27c517a6088520c282715321d4f013fd84
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-09-20 14:40:14 -0400

    Create 6kyu_FindTheMissingLetter.cs

---
+++ 6kyu_FindTheMissingLetter.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Kata
+{
+  public static int FindMissingLetter(char[] array)
+  {
+    var actualSum = array.ToList().Sum(chr => (int)chr);
+    var expectedSum = Enumerable.Range((int)array[0], array.Length+1).Sum();
+    return (char)(expectedSum - actualSum);
+  }
+}
+
+// https://www.codewars.com/kata/5839edaa6754d6fec10000a2
