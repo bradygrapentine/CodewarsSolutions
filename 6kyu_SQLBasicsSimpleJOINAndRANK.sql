e7b044717d0a494bf53071353098b7c58a6154db
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-09-23 12:49:42 -0400

    Update 6kyu_SQLBasicsSimpleJOINAndRANK.sql

--- 6kyu_SQLBasicsSimpleJOINAndRANK.sql
+++ 6kyu_SQLBasicsSimpleJOINAndRANK.sql
@@ -1,9 +1,16 @@
-SELECT people.*,
-COUNT(sales.id) AS sale_count,
-RANK() OVER (ORDER BY COUNT(sales.id) DESC) sale_rank
-FROM people
-JOIN sales ON sales.people_id=people.id
-GROUP BY people.id
-ORDER BY sale_count DESC
+SELECT p.*,
+COUNT(s) AS sale_count,
+RANK() OVER (ORDER BY COUNT(s) DESC) sale_rank
+FROM people AS p
+JOIN sales AS s ON s.people_id=p.id
+GROUP BY p.id
+
+-- SELECT people.*,
+-- COUNT(sales.id) AS sale_count,
+-- RANK() OVER (ORDER BY COUNT(sales.id) DESC) sale_rank
+-- FROM people
+-- JOIN sales ON sales.people_id=people.id
+-- GROUP BY people.id
+-- ORDER BY sale_count DESC
 
 -- https://www.codewars.com/kata/58094559c47d323ebd000035
