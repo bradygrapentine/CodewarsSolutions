b6cc7aee04cb6ae2d22f383209186402df91fc12
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-09-24 16:51:47 -0400

    Update 6kyu_SQLBasicsSimpleEXISTS.sql

--- 6kyu_SQLBasicsSimpleEXISTS.sql
+++ 6kyu_SQLBasicsSimpleEXISTS.sql
@@ -1,9 +1,14 @@
-SELECT d.id AS id, d.name as name
-FROM departments AS d
-JOIN sales AS s on s.department_id = d.id
-WHERE EXISTS
-(SELECT price FROM sales WHERE s.price > 98.00)
-GROUP BY d.id
-ORDER BY d.id; 
+SELECT departments.*
+FROM departments
+WHERE EXISTS (SELECT * FROM sales WHERE sales.price > 98.00 
+                                  AND sales.department_id = departments.id);
+
+-- SELECT d.id AS id, d.name as name
+-- FROM departments AS d
+-- JOIN sales AS s on s.department_id = d.id
+-- WHERE EXISTS
+-- (SELECT price FROM sales WHERE s.price > 98.00)
+-- GROUP BY d.id
+-- ORDER BY d.id; 
 
 -- https://www.codewars.com/kata/58113a64e10b53ec36000293
