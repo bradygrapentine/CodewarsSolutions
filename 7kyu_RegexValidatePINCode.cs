9d4aebd6db74ba362de594a73a4157f0a6df3e55
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-09-27 11:49:15 -0400

    Update 7kyu_RegexValidatePINCode.cs

--- 7kyu_RegexValidatePINCode.cs
+++ 7kyu_RegexValidatePINCode.cs
@@ -5,13 +5,25 @@ public class Kata
 {
   public static bool ValidatePin(string pin)
   {
-    var pattern1 = new Regex(@"^\d{4}$");
-    var pattern2 = new Regex(@"^\d{6}$");
-    foreach (var chr in pin) {
-      if (chr == '\n') return false;
-    }
-    return pattern1.IsMatch(pin) ? true: pattern2.IsMatch(pin) ? true: false;
+    var pattern = new Regex(@"^[\d]{4,6}\z");
+    return pin.Length == 5 ? false: pattern.IsMatch(pin) ? true: false;
   }
 }
+
+// using System;
+// using System.Text.RegularExpressions;
+
+// public class Kata
+// {
+//   public static bool ValidatePin(string pin)
+//   {
+//     var pattern1 = new Regex(@"^\d{4}$");
+//     var pattern2 = new Regex(@"^\d{6}$");
+//     foreach (var chr in pin) {
+//       if (chr == '\n') return false;
+//     }
+//     return pattern1.IsMatch(pin) ? true: pattern2.IsMatch(pin) ? true: false;
+//   }
+// }
 
 // https://www.codewars.com/kata/55f8a9c06c018a0d6e000132
