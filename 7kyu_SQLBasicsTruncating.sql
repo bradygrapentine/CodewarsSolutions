38045f0b59895fec148d9ca7397b2935bc99c937
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-09-29 13:47:13 -0400

    Create 7kyu_SQLBasicsTruncating.sql

---
+++ 7kyu_SQLBasicsTruncating.sql
@@ -0,0 +1,11 @@
+SELECT TRUNC(number1 + number2) AS towardzero
+FROM decimals
+
+SELECT 
+  CASE
+    WHEN (number1 + number2) > 0 THEN FLOOR(number1 + number2)
+    ELSE CEIL(number1 + number2)
+  END AS towardzero
+FROM decimals
+
+-- https://www.codewars.com/kata/594a8fa5a2db9e5f290000c3
