4c09451a082719baf27d17d38f4c78a0c5e6685d
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-10-04 11:42:12 -0400

    Create 6kyu_ConsecutiveStrings.js

---
+++ 6kyu_ConsecutiveStrings.js
@@ -0,0 +1,12 @@
+function longestConsec(strarr, k) {
+  if ((strarr.length === 0 || strarr === null) || k > strarr.length || k <= 0) { 
+    return ""
+  }
+  let longest = ""
+  for (let i = 0; i <= strarr.length-k; i++) {
+    longest = strarr.slice(i,i+k).join('').length > longest.length ? strarr.slice(i,i+k).join('') : longest
+  }
+  return longest
+}
+
+// https://www.codewars.com/kata/56a5d994ac971f1ac500003e
