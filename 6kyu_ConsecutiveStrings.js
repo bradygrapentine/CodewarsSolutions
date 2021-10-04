2b7402dab8c16c9110e02569d6a4a5d06ea27843
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-10-04 11:45:43 -0400

    Update 6kyu_ConsecutiveStrings.js

--- 6kyu_ConsecutiveStrings.js
+++ 6kyu_ConsecutiveStrings.js
@@ -1,12 +1,21 @@
 function longestConsec(strarr, k) {
-  if ((strarr.length === 0 || strarr === null) || k > strarr.length || k <= 0) { 
-    return ""
-  }
+  if (k <= 0) return ""
   let longest = ""
   for (let i = 0; i <= strarr.length-k; i++) {
     longest = strarr.slice(i,i+k).join('').length > longest.length ? strarr.slice(i,i+k).join('') : longest
   }
   return longest
 }
+
+// function longestConsec(strarr, k) {
+//   if ((strarr.length === 0 || strarr === null) || k > strarr.length || k <= 0) { 
+//     return ""
+//   }
+//   let longest = ""
+//   for (let i = 0; i <= strarr.length-k; i++) {
+//     longest = strarr.slice(i,i+k).join('').length > longest.length ? strarr.slice(i,i+k).join('') : longest
+//   }
+//   return longest
+// }
 
 // https://www.codewars.com/kata/56a5d994ac971f1ac500003e
