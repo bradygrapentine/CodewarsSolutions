abcb36aa116ca786ad3b66213eb62d0941a3fd66
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-10-05 10:08:47 -0400

    Create 6kyu_DuplicateEncoder.js

---
+++ 6kyu_DuplicateEncoder.js
@@ -0,0 +1,18 @@
+function duplicateEncode(word){
+  let result = ""
+  word = word.toLowerCase()
+  let counts = {}
+  for (var i = 0; i < word.length; i++) {
+    if (counts[word[i]] === undefined) {
+      counts[word[i]] = 1
+    } else {
+      counts[word[i]] += 1
+    }
+  }
+  for (var i = 0; i < word.length; i++) {
+    result = counts[word[i]] > 1 ? result += ')' : result += '('
+  }
+  return result
+}
+
+// https://www.codewars.com/kata/54b42f9314d9229fd6000d9c
