ba5691ee53e44fb2abd9bf2fb10f9f47e09cb5a6
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-10-05 18:57:22 -0400

    Update 5kyu_NotVerySecure.js

--- 5kyu_NotVerySecure.js
+++ 5kyu_NotVerySecure.js
@@ -1,7 +1,15 @@
 function alphanumeric(string){
-  if (string == "") return false
-  const pattern = new RegExp('^[A-Z|a-z|0-9]+$')
-  return pattern.test(string)
+  return /^[a-z|0-9]+$/i.test(string)
 }
+
+// function alphanumeric(string){
+//   return /^[A-Z|a-z|0-9]+$/.test(string)
+// }
+
+// function alphanumeric(string){
+//   if (string == "") return false
+//   const pattern = new RegExp('^[A-Z|a-z|0-9]+$')
+//   return pattern.test(string)
+// }
 
 // https://www.codewars.com/kata/526dbd6c8c0eb53254000110
