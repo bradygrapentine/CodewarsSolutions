c210d96f3d47d79d47b80c1ef33e07f5c49c1406
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-10-05 10:16:55 -0400

    Update 6kyu_DuplicateEncoder.js

--- 6kyu_DuplicateEncoder.js
+++ 6kyu_DuplicateEncoder.js
@@ -1,18 +1,31 @@
 function duplicateEncode(word){
   let result = ""
   word = word.toLowerCase()
-  let counts = {}
   for (var i = 0; i < word.length; i++) {
-    if (counts[word[i]] === undefined) {
-      counts[word[i]] = 1
+    if (word.indexOf(word[i]) === word.lastIndexOf(word[i])) {
+      result += '('
     } else {
-      counts[word[i]] += 1
+      result += ')'
     }
-  }
-  for (var i = 0; i < word.length; i++) {
-    result = counts[word[i]] > 1 ? result += ')' : result += '('
   }
   return result
 }
+
+// function duplicateEncode(word){
+//   let result = ""
+//   word = word.toLowerCase()
+//   let counts = {}
+//   for (var i = 0; i < word.length; i++) {
+//     if (counts[word[i]] === undefined) {
+//       counts[word[i]] = 1
+//     } else {
+//       counts[word[i]] += 1
+//     }
+//   }
+//   for (var i = 0; i < word.length; i++) {
+//     result = counts[word[i]] > 1 ? result += ')' : result += '('
+//   }
+//   return result
+// }
 
 // https://www.codewars.com/kata/54b42f9314d9229fd6000d9c
