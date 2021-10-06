66d7273d7ad6c4952a5fe31fc71f8a3bb853df5e
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-10-06 08:57:55 -0400

    Update 6kyu_YourOrderPlease.js

--- 6kyu_YourOrderPlease.js
+++ 6kyu_YourOrderPlease.js
@@ -1,13 +1,17 @@
-function order(words){
-  if (words === "") return "" 
-  const regex = /[0-9]/g;
-  let wordArray = words.split(' ')
-  let result = []
-  for (var i = 0; i < wordArray.length; i++) {
-    let newIndex = Number(wordArray[i].match(regex));
-    result[newIndex-1] = wordArray[i]
-  }
-  return result.join(' ')
+function order(words) {
+  return words.split(' ').sort((a,b) => a.match(/[0-9]/)-b.match(/[0-9]/)).join(' ')
 }
+
+// function order(words){
+//   if (words === "") return "" 
+//   const regex = /[0-9]/;
+//   let wordArray = words.split(' ')
+//   let result = []
+//   for (var i = 0; i < wordArray.length; i++) {
+//     let newIndex = Number(wordArray[i].match(regex));
+//     result[newIndex-1] = wordArray[i]
+//   }
+//   return result.join(' ')
+// }
 
 // https://www.codewars.com/kata/55c45be3b2079eccff00010f
