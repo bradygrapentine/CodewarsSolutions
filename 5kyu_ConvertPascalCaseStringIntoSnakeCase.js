ad4a95d9a0707ed184107154f3591a15d8e34b4f
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-10-07 20:20:28 -0400

    Update 5kyu_ConvertPascalCaseStringIntoSnakeCase.js

--- 5kyu_ConvertPascalCaseStringIntoSnakeCase.js
+++ 5kyu_ConvertPascalCaseStringIntoSnakeCase.js
@@ -1,20 +1,24 @@
 function toUnderscore(string) {
-  if (typeof string === "number") {
-    return string.toString()
-  }
-  let nums = /[0-9]/
-  let stringAsArr = string.split('')
-  for (var  i = 0; i < stringAsArr.length; i++) {
-    if (nums.test(stringAsArr[i])) {
-      continue;
-    } else if (i === 0) {
-      stringAsArr[i] = stringAsArr[i].toLowerCase()  
-    } else if (stringAsArr[i] === stringAsArr[i].toUpperCase()) {
-      stringAsArr.splice(i, 1, ('_' + stringAsArr[i].toLowerCase()))
-      i++
-    }
-  }
-  return stringAsArr.join('')
+  return typeof string === "number" ? string.toString() : string.split(/(?=[A-Z])/).join('_').toLowerCase()
 }
+
+// function toUnderscore(string) {
+//   if (typeof string === "number") {
+//     return string.toString()
+//   }
+//   let nums = /[0-9]/
+//   let stringAsArr = string.split('')
+//   for (var  i = 0; i < stringAsArr.length; i++) {
+//     if (nums.test(stringAsArr[i])) {
+//       continue;
+//     } else if (i === 0) {
+//       stringAsArr[i] = stringAsArr[i].toLowerCase()  
+//     } else if (stringAsArr[i] === stringAsArr[i].toUpperCase()) {
+//       stringAsArr.splice(i, 1, ('_' + stringAsArr[i].toLowerCase()))
+//       i++
+//     }
+//   }
+//   return stringAsArr.join('')
+// }
 
 // https://www.codewars.com/kata/529b418d533b76924600085d
